95e0f5aab3790ee880d880a02f8508574510738a
JULIEN LI <31632821+JulienL-Deca@users.noreply.github.com>
2018-04-09 18:52:52 +0200

    exercice done square_digits [02_programming_fundamentals/05_callbacks/03_square_digits]

--- 02_programming_fundamentals/05_callbacks/03_square_digits/square_digits.js
+++ 02_programming_fundamentals/05_callbacks/03_square_digits/square_digits.js
@@ -6,11 +6,60 @@
 //
 // Note: This is not just the square of a number but the square of each digits
 // Note: The function accepts an integer and returns an integer
-
 function squareDigits(number) {
   // Your code here
-}
+  if (number) {
+    const digits =
+      number
+        .toString()
+        .split("")
+        .map((digitString) => parseInt(digitString, 10))
+        .map((digit) => digit * digit)
+        .join("");
 
+    return parseInt(digits, 10);
+  } else {
+    //console.log(`dans false ${number}`);
+  }
+
+}
+/*
+function squareDigits(number) {
+  // Your code here
+  if (number) {
+    const numberString = number.toString();
+    const digits =
+      numberString
+        .split("")
+        .map((digitString) => parseInt(digitString, 10));
+    const digitsSquared = digits.map((digit) => digit * digit);
+
+    const digitsJoined = digitsSquared.join("");
+
+    return parseInt(digitsJoined, 10);
+  } else {
+    //console.log(`dans false ${number}`);
+  }
+
+}
+*/
+/* fonctionne pour un argument avec 1 seul chiffre et qui gère si on ne donne pas de chiffre
+function squareDigits(number) {
+  // Your code here
+  let newString = "", tempString = "", tempValue = 0;
+  if (number) {
+    number.toString();
+    tempValue = square(number);
+    tempString += tempValue.toString();
+
+    newString = tempString;
+
+  } else {
+    //console.log(`dans false ${number}`);
+  }
+  return newString;
+}
+*/
 // Do not remove last lines, it is for tests
 // eslint-disable-next-line
 module.exports = squareDigits;

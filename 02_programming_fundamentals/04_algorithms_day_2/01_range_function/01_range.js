738a65107efe0fd8dd7d55af975213ee6d142e70
JULIEN LI <31632821+JulienL-Deca@users.noreply.github.com>
2018-04-10 00:49:14 +0200

    exercice ragnge function [02_programming_fundamentals/04_algorithms_day_2/01_range_function]

--- 02_programming_fundamentals/04_algorithms_day_2/01_range_function/01_range.js
+++ 02_programming_fundamentals/04_algorithms_day_2/01_range_function/01_range.js
@@ -4,8 +4,18 @@
 // This function should work in both ascending or descending order.
 
 // Complete this function.
-function range() {
-
+function range(nb1, nb2) {
+  let myArrayOfNumber = [];
+  if (nb1 < nb2) {
+    for (let i = nb1; i <= nb2; i++) {
+      myArrayOfNumber.push(i);
+    }
+  } else { // nb1 > nb2
+    for (let i = nb1; i >= nb2; i--) {
+      myArrayOfNumber.push(i);
+    }
+  }
+  return myArrayOfNumber;
 }
 
 // Do not remove last lines, it is for tests

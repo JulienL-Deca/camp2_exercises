32ca5083dd81dbcc678ca4e9ee6c3f101364dc6d
JULIEN LI <31632821+JulienL-Deca@users.noreply.github.com>
2018-04-09 13:56:19 +0200

    exercice create a callback [02_programming_fundamentals/05_callbacks/02_create_callbacks]

--- 02_programming_fundamentals/05_callbacks/02_create_callbacks/create_callback.js
+++ 02_programming_fundamentals/05_callbacks/02_create_callbacks/create_callback.js
@@ -2,8 +2,9 @@ const log = (text) => console.log(text);
 
 // A function that takes two parameters, this first one being an object like below
 // and the second one being a callback function.
-const getInput = (____, ____) => {
+const getInput = (myObject, callback) => {
   // When finished, we want to log the key params...
+  callback(myObject.key);
 };
 
 getInput({user: "Lorem", key: "arrow_up"}, log);

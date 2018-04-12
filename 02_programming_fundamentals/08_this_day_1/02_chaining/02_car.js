dfa4259451e5cf256a6ea7a46f2556ef30ba29cd
JULIEN LI <31632821+JulienL-Deca@users.noreply.github.com>
2018-04-12 14:27:30 +0200

    exercice O2_chaining [02_programming_fundamentals/08_this_day_1/02_chaining]

--- 02_programming_fundamentals/08_this_day_1/02_chaining/02_car.js
+++ 02_programming_fundamentals/08_this_day_1/02_chaining/02_car.js
@@ -1,3 +1,36 @@
 // See Sparta courses for the exercise summary
+/*
+Create an object car with four functions:
+start() will reset all the car data
+changeSpeed(speed) will change the km/h speed of the car
+drive(minutes) will make your car drive at the previously set speed for that amount of time in minutes
+showDistance() will print the distance you drove as the number of kilometers
+Take care to stock that data inside the object. For instance the code
+car.start().changeSpeed(130).drive(42).showDistance();
+should display 91 Km.
+*/
+const car = {
+  driveAtSpeed: -1,
+  driveForXMinutes: -1,
+  globalDistance: -1,
+  start: function() {
+    this.driveAtSpeed = 0;
+    this.driveForXMinutes = 0;
+    this.globalDistance = 0;
+    return this;
+  },
+  changeSpeed: function(newSpeed) {
+    this.driveAtSpeed = newSpeed;
+    return this;
+  },
+  drive: function(newDurationOfDrive) {
+    this.driveForXMinutes = newDurationOfDrive;
+    this.globalDistance += (this.driveForXMinutes / 60) * this.driveAtSpeed;
+    return this;
+  },
+  showDistance: function() {
+    console.log(`${this.globalDistance} Km`);
+  }
+};
 
 module.exports = car;

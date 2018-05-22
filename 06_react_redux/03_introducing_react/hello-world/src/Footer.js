a640d576c66997d9b4c03b3d96cdb8639785639c
JULIEN LI <31632821+JulienL-Deca@users.noreply.github.com>
2018-05-22 14:24:13 +0200

    1er proj en react [06_react_redux/03_introducing_react]

---
+++ 06_react_redux/03_introducing_react/hello-world/src/Footer.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="Footer">
+      <table align="right">
+        <tr>
+          <td><h3 className="App-foot">This is my footer</h3></td>
+        </tr>
+      </table>
+      </div>
+    );
+  }
+}
+
+export default Footer;

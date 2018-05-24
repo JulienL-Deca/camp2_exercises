69c850b481d7f07d272843a1e2ca3a737bf24bc5
JULIEN LI <31632821+JulienL-Deca@users.noreply.github.com>
2018-05-24 20:20:28 +0200

    getting data from 2 tables, merg and search [06_react_redux/04_lifecycle_method/02_transform_data]

--- 06_react_redux/04_lifecycle_method/02_transform_data/src/App.js
+++ 06_react_redux/04_lifecycle_method/02_transform_data/src/App.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+function findUser(users, id) {
+  const userFound = users.find((user) => {
+    return user.id === id;
+  });
+  return (userFound) ? userFound.name : "No one";
+}
+
+function displayExpenses(props){
+  return props.expenses.map((expense, index) => {
+    return (
+      <tr key={index}>
+        <td>{expense.what}</td>
+        <td>{findUser(props.users, expense.who)}</td>
+      </tr>
+    );
+  })
+}
 
 // App has `expenses` and `users` as props
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <table border="1px solid black">
+          <thead>
+            <tr>
+              <th>what has been paid</th>
+              <th>by whom?</th>
+            </tr>
+          </thead>
+          <tbody>
+            {displayExpenses(this.props)}
+          </tbody>
+        </table>
       </div>
     );
   }
